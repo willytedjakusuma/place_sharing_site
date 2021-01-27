77a8a6a4d1795d529dc05598ef244b0f56134102
Willy Tedjakusuma <willy.tedjakusuma@gmail.com>
2021-01-28 01:16:31 +0700

    feat(user): add /:username route
    naming a route for user show

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "dashboard#index"
+
+  resources :locations
+  resources :users, only: [:index]
+  get ':username', to: 'users#show', as: :user_profile
 end
