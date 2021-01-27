cf99f9285b51dc3645feb8e23a4b042ca7314a09
Willy Tedjakusuma <willy.tedjakusuma@gmail.com>
2021-01-28 01:18:32 +0700

    feat(database): database setup
    database setup

--- db/schema.rb
+++ db/schema.rb
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2021_01_24_153558) do
 
   create_table "users", force: :cascade do |t|
     t.string "display_name"
+    t.string "username"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
